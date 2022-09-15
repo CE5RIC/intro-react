1df4e2a5c32d6bc4d71d06297028b125147b91bd
CE5RIC <cedricdierckx5@hotmail.com>
2022-09-15 12:11:24 +0200

    mapping over array and use of key

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,14 +1,17 @@
+import {useState} from "react";
+
 const Todos = () => {
+    const initialTodos = ["My first todo", "My second todo", "My third todo"];
+    const [todos, setTodos] = useState(initialTodos);
     return (
-        <div>
-
-            <ul>Learn React</ul>
-            <input type="checkbox"/>
-            <ul>Be Awesome</ul>
-            <input type="checkbox"/>
-        </div>
-
-    )
+        <ul>
+            {todos.map((todo) => (
+                <li key={todo}>
+                    <input type="checkbox" /> {todo}
+                </li>
+            ))}
+        </ul>
+    );
 }
 
 export default Todos;
