7ffad4cd31235a96a49252c2e275b2a926793b04
CE5RIC <cedricdierckx5@hotmail.com>
2022-09-15 10:12:48 +0200

    Todo list component created

---
+++ src/components/Todos.js
@@ -0,0 +1,14 @@
+const Todos = () => {
+    return (
+        <div>
+
+            <ul>Learn React</ul>
+            <input type="checkbox"/>
+            <ul>Be Awesome</ul>
+            <input type="checkbox"/>
+        </div>
+
+    )
+}
+
+export default Todos;
