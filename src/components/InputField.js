ef642b3f781dd67b604be472824bdefec7542c0f
CE5RIC <cedricdierckx5@hotmail.com>
2022-09-15 09:49:40 +0200

    Inputfield component created

---
+++ src/components/InputField.js
@@ -0,0 +1,10 @@
+const InputField = () => {
+    return (
+        <div className="input">
+
+            <input placeholder="Type a new todo" type="text" id="todo" name="todo"/>
+        </div>
+    )
+}
+
+export default InputField;
