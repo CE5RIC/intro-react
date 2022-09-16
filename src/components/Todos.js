4b5a55814f52e387eba5910283120fa92c475e85
CE5RIC <cedricdierckx5@hotmail.com>
2022-09-16 14:50:55 +0200

    Todo checkbox fixed using state

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,17 +1,41 @@
 import {useState} from "react";
 
 const Todos = () => {
-    const initialTodos = ["My first todo", "My second todo", "My third todo"];
+    const initialTodos = [
+        {
+            name: "First thing to do",
+        },
+        {
+            name: "Second thing to do",
+        },
+        {
+            name: "Third thing to do",
+        }
+    ];
+
     const [todos, setTodos] = useState(initialTodos);
+    const [checkedState, setCheckedState] = useState(new Array(initialTodos.length).fill(false)
+    );
+
+    const handleOnChange = (position) => {
+        const updatedCheckedState = checkedState.map((item, index) =>
+            index === position ? !item : item)
+
+        setCheckedState(updatedCheckedState);
+
+    };
+
+
     return (
         <ul>
-            {todos.map((todo) => (
-                <li key={todo}>
-                    <input type="checkbox" /> {todo}
+            {todos.map(({name}, index) => (
+                <li key={index}>
+                    <input type="checkbox" name={name} value={name} checked={checkedState[index]} onChange={() => handleOnChange(index)} />
+                    <label>{name}</label>
                 </li>
             ))}
         </ul>
     );
 }
 
-export default Todos;
+export default Todos;;
